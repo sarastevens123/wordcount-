9ff876b810aac7fc09eb50febd9dd5aa3aade15b
Sara Stevens <sarastevens@Saras-MacBook-Air.local>
2022-08-16 20:45:47 -0700

    created tokenize function

--- wordcount.py
+++ wordcount.py
@@ -2,3 +2,53 @@
 
 
 # put your code here.
+def tokenize(file):
+  """Returns a list of words from the file"""
+  open_file = open(file)
+  list_of_words = []
+  for line in open_file:
+    line = line.rstrip()
+    words = line.split(' ')
+
+    for word in words:
+      if not word:
+        continue
+      if word[-1] in ',.?!)*':
+        word = word[:-1]
+      elif word[0] in ',.?!)*':
+        word = word[1:]
+      
+      list_of_words.append(word)
+
+  open_file.close()
+  return list_of_words
+print(tokenize('test.txt'))
+
+
+
+# def count_words(file):
+#     word_count = {}
+
+#         for word in words:
+          
+#             print(word)
+#             if not word:
+#               continue
+
+#             if word[-1] in ',.?!)*':
+#                 word = word[:-1]
+#             elif word[0] in ',.?!)*':
+#                 word = word[1:]
+            
+#             word = word.upper()
+            
+#             if word not in word_count:
+#                 word_count[word]= 1
+#             elif word in word_count:
+#                 word_count[word] += 1
+    
+#     for key, value in word_count.items():
+#       print(f"{key} {value}")
+#     open_file.close()
+
+# print(count_words('twain.txt'))
